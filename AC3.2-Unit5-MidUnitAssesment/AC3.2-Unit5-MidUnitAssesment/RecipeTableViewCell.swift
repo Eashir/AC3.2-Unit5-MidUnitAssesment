948039beb46e1eb8751310704b72722cb88537fd
Eashir Arafat <eashirarafat@gmail.com>
2017-01-03 18:04:32 -0500

    eh

---
+++ AC3.2-Unit5-MidUnitAssesment/AC3.2-Unit5-MidUnitAssesment/RecipeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RecipeTableViewCell.swift
+//  AC3.2-Unit5-MidUnitAssesment
+//
+//  Created by Eashir Arafat on 1/3/17.
+//  Copyright © 2017 C4Q. All rights reserved.
+//
+
+import UIKit
+
+class RecipeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
